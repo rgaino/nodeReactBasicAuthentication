7ae7e18e9cf21c22f274fb066941216d28b5444b
Rafael Gaino <rgaino@gmail.com>
2018-05-29 17:57:30 -0300

    Installed Sequelize and connected to mysql

--- server/index.js
+++ server/index.js
@@ -1,3 +1,19 @@
+//Setting up database connection
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  "mysql://root:hYhzHh8LrXTcZXcWbbppAgrg@127.0.0.1:3306/fregues-dev"
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+//Setting up Express routing
 const express = require("express");
 const app = express();
 
