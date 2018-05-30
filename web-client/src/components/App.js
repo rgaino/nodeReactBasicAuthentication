fdb7c0648202786f6d73244494a82771fc86af9e
Rafael Gaino <rgaino@gmail.com>
2018-05-30 10:31:31 -0300

    Basic routing structure

--- web-client/src/components/App.js
+++ web-client/src/components/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import Landing from "./Landing";
+import Signup from "./Signup";
 
 class App extends Component {
   render() {
-    return <div>Oi</div>;
+    return (
+      <div>
+        {" "}
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/signup" component={Signup} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
